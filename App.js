11d98484b70cf7cc414a6b5791f0d2af5aa728c3
Md Mansoor Ahmed <ahmed.ans123@gmail.com>
2024-03-22 23:04:55 +0530

    Ep-04

--- App.js
+++ App.js
@@ -1,20 +1,116 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React CDN"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import restaurants from "/data.js";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { className: "child" }, "This is child 1"),
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h1", {}, "This is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", { className: "child" }, "This is child 2"),
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h1", {}, "This is h2 tag"),
-  ]),
-]);
+const Header = () => {
+  return (
+    <div id="header">
+      <img
+        id="logo"
+        src="https://logowik.com/content/uploads/images/t_free-food-delivery6258.logowik.com.webp"
+      ></img>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+const Search = () => {
+  return (<div id="search">
+    <input type="text"></input>
+    <button>Search</button>
+  </div>)
+};
+// const RestaurantCard = (props) => {
+//   const { name, area, cuisines, rating, image, cost } = props.RestData;
+//   return (
+//     <div className="card">
+//       <img className="restImage"
+//         src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + image}
+//       ></img>
+//       <h4>Name: {name}</h4>
+//       <h4>Area: {area}</h4>
+//       <h4>Cuisines: {cuisines.join(", ")}</h4>
+//       <h4>Avg Rating: {rating}</h4>
+//       <h4>{cost}</h4>
+//     </div>
+//   );
+// };
+
+// const RestaurantContainer = () => {
+//   return (
+//     <div id="container">
+//       {
+//         restaurants.map(rest =>
+//         (<RestaurantCard key={rest.info.id}
+//           RestData={{
+//             name: rest.info.name,
+//             area: rest.info.areaName,
+//             cuisines: rest.info.cuisines,
+//             rating: rest.info.avgRating,
+//             image: rest.info.cloudinaryImageId,
+//             cost: rest.info.costForTwo
+//           }}
+//         />)
+//         )
+//       }
+//     </div>
+//   );
+// };
+
+// Refactored code
+const RestaurantCard = (props) => {
+  const { name, areaName, cuisines, avgRating, cloudinaryImageId, costForTwo } = props.RestData.info;
+  return (
+    <div className="card">
+      <img className="restImage"
+        src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" + cloudinaryImageId}
+      ></img>
+      <h4>Name: {name}</h4>
+      <h4>Area: {areaName}</h4>
+      <h4>Cuisines: {cuisines.join(", ")}</h4>
+      <h4>Avg Rating: {avgRating}</h4>
+      <h4>{costForTwo}</h4>
+    </div>
+  );
+};
+
+const RestaurantContainer = () => {
+  return (
+    <div id="container">
+      {
+        restaurants.map(rest =>
+          <RestaurantCard key={rest.info.id} RestData={rest} />
+        )
+      }
+    </div>
+  );
+};
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div id="footer">
+        <p>made with Love</p>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Search />
+      <RestaurantContainer />
+      <Footer />
+    </>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// root.render(heading);
+root.render(<AppLayout />);
